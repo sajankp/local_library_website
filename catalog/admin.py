11001b4d9c50c3c984706791e4bd5f1e0b69e215
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-05-29 17:45:43 +0530

    Add code from complete part 4 including challenges.
    added inline option to admin interface for book,author

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,24 +9,41 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 # Define the admin class
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+    # fields = ['title', 'isbn','genre']
+    exclude = ['summary']
+    def has_change_permission(self,request, obj=None):
+        return False
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+    def has_change_permission(self,request, obj=None):
+        return False
+
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-    list_display = ("book","id","status")
+    list_display = ("book","status","id","due_back")
     fieldsets = (
         (None,{
             'fields':('book','imprint','id')
