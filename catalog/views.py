eec3fdcc92b506e4a21d799f9678dd7e0995bdce
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-06-04 15:49:51 +0530

    Add list and then detail view for books into index and create url,view and template for the same

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -29,3 +30,10 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
